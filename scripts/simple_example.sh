5f47e7e723ac2eb1092e41e7e7a2466fdfbcdfe6
Ines Arous <ines@exascale.info>
2020-01-21 13:21:11 +0100

    updating the example

---
+++ scripts/simple_example.sh
@@ -0,0 +1,17 @@
+python3 ../src/var_em.py \
+		--labeled_social_features '../input/simple_example_vem_labeled.csv' \
+		--unlabeled_social_features '../input/simple_example_vem_unlabeled.csv'\
+		--annotation_matrix '../input/aij_simple_example_vem.csv'\
+		--labels '../input/simple_example_vem_labels.csv'\
+		--total_epochs_nn 100\
+		--total_neurons_nn 10\
+		--steps 1\
+		--supervision_rate 0.3\
+		--nb_iterations_EM 20\
+		--sampling_rate 10.0\
+		--worker_reliability_file '../output/simple_example/worker_reliability/worker_reliability_sup_60_sr_10_0_name.csv'\
+		--influencer_quality_file '../output/simple_example/influencer_quality/influencer_quality_sup_60_sr_10_name.csv'\
+		--evaluation_file '../output/simple_example/evaluation/evaluation_sup_60_sr_10_0_name.csv'\
+		--theta_file '../output/simple_example/theta/theta_i0_sup_60_sr_10_0_name.csv'\
+		--weights_before_em '../output/simple_example/weights_before/weights_before_em_sup_60_sr_10_0_name.csv'\
+		--weights_after_em '../output/simple_example/weights_after/weights_after_em_sup_60_sr_10_0_name.csv'\
